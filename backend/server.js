513ddba849d089376150c3bfb6e0ce66364ddc27
Bryan Lomerio <bryanlomerioaninogithub@gmail.com>
2024-12-15 06:56:10 +0800

    fix: resolve image upload issue

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 const blogPostRoutes = require('./routes/blogPostRoutes');
 
 const app = express();
+
 app.use(cors());
+
 app.use(bodyParser.json());
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/api/blogposts', blogPostRoutes);
 
